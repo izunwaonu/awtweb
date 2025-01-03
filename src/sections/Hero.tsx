65cf2ae0cc5f62754df1c1157e16ded9cbea22f9
swaat <izunwaonu2@gmail.com>
2025-01-03 21:42:48 +0700

    Updated responsiveness

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -5,13 +5,13 @@ import cogMain from "@/assets/cogmain.png";
 import noodleImage from "@/assets/noodle.png";
 export const Hero = () => {
   return (
-    <section className="pt-8 pb-20 md:p-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip">
+    <section className="pr-5 pt-8 pb-20 md:p-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip">
       <div className="container">
         <div className="md:flex items-center">
-          <div className="md: w-[478px]"> 
+          <div className="w-[300px] md:w-[470px]"> 
               <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">Sefora: Now Available</div>
               <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#48CAE4] text-transparent bg-clip-text mt-6">Safety, Connection, Empowerment</h1>
-              <p className="text-xl text-[#010D3E] tracking-tight mt-6">
+              <p className="text-xl sm:text-sm md:text-sm text-[#010D3E] tracking-tight mt-6">
               Celebrate Safora App, a safe space for family security, 
               social interaction, and guidance. 
               Stay connected, protected, and guided—all in one app.
@@ -25,10 +25,10 @@ export const Hero = () => {
               </div>
           </div>
       
-        <div className="mt-20 md:-mt-0 md:h-[648px] md:flex-1 relative">
-          <Image src={cogMain} alt="cog" className="md:absolute  md:width-auto md:max-w-none md:-left-6 lg:left-0" />
+        <div className="mt-20 md:mt-0 md:ml-10 md:h-[648px] md:flex-1 relative">
+          <Image src={cogMain} alt="cog" className=" opacity-100 md:absolute md:mt-[70px] md:width-auto md:max-w-none md:-left-6 lg:left-0" />
           <Image src={cogImage} alt="Img" width={220} height={220} className="hidden md:block -top-8 -left-32 md:absolute"/>
-          <Image src={noodleImage} alt="Img" width={220} className="hidden lg:block absolute top-[524px] left-[448px] rotate-[30deg]"/>
+          <Image src={cogImage} alt="Img" width={220} className="lg:block absolute top-[524px] left-[448px] rotate-[30deg]"/>
         </div>
         </div>
       </div>
