aaf6d9c5bb140f838a0f89d2c47b9b4093303153
swaat <izunwaonu2@gmail.com>
2025-01-03 22:13:27 +0700

    updated

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -8,7 +8,7 @@ export const Hero = () => {
     <section className="pr-5 pt-8 pb-20 md:p-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip">
       <div className="container">
         <div className="md:flex items-center">
-          <div className="w-[300px] md:w-[470px]"> 
+          <div className="w-[320px] md:w-[470px]"> 
               <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">Sefora: Now Available</div>
               <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#48CAE4] text-transparent bg-clip-text mt-6">Safety, Connection, Empowerment</h1>
               <p className="text-xl sm:text-sm md:text-sm text-[#010D3E] tracking-tight mt-6">
@@ -27,7 +27,7 @@ export const Hero = () => {
       
         <div className="mt-20 md:mt-0 md:ml-10 md:h-[648px] md:flex-1 relative">
           <Image src={cogMain} alt="cog" className=" opacity-100 md:absolute md:mt-[70px] md:width-auto md:max-w-none md:-left-6 lg:left-0" />
-          <Image src={cogImage} alt="Img" width={220} height={220} className="hidden md:block -top-8 -left-32 md:absolute"/>
+          <Image src={cogImage} alt="Img" width={220} height={220} className="md:block block -top-8 -left-32 md:absolute"/>
           <Image src={cogImage} alt="Img" width={220} className="lg:block absolute top-[524px] left-[448px] rotate-[30deg]"/>
         </div>
         </div>
