2c17dde43b5911f163187c9ee5034514137ff10b
swaat <izunwaonu2@gmail.com>
2025-01-10 22:10:17 +0700

    updated

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,3 +1,6 @@
+import CheckIcon from "@/assets/check.svg";
+import {twMerge} from "tailwind-merge";
+
 const pricingTiers = [
   {
     title: "Free",
@@ -51,5 +54,47 @@ const pricingTiers = [
 ];
 
 export const Pricing = () => {
-  return null;
+  return (
+    <section className="py-8">
+      <div className="container">
+        <h2 className="section-title">Pricing</h2>
+        <p className="section-description mt-5">
+          Our pricing plans are flexible and designed to meet the needs of individuals and...
+        </p>
+          <div className="flex flex-col gap-6 items-center mt-10">
+           {
+            pricingTiers.map(({title, monthlyPrice, buttonText, popular, inverse, features})=>(
+              <div className={twMerge("p-10 border border-[#F1F1F1] rounded-3xl shadow-[0_7px_14px_#EAEAEA] max-w-xs ", inverse === true && 'border-black bg-black text-white/60')}>
+                <div className="flex justify-between">
+                  <h3 className={twMerge("text-lg font-bold text-black/50", inverse === true && "text-white/60")} key={title}>{title}</h3>
+                  {
+                    popular === true && (
+                      <div className="inline-flex text-sm px-4 py-1.5 rounded-xl border border-white/20">
+                      <span className="bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#EBCB92,#71C2EF,#3BFFFF,#DD7DDF)] text-transparent bg-clip-text font-medium"> Popular</span>
+                      </div>
+                    )
+                  }
+                </div>
+                <div className="flex items-baseline gap-1 mt-[30px]">
+                  <span className="text-4xl font-bold tracking-tighter leading-none">${monthlyPrice}</span>
+                  <span className="tracking-tighter font-bold text-black/50 ">/month</span>
+                </div>
+                <button className={twMerge("btn btn-primary w-full mt-[30px]", inverse === true && "bg-white text-black")}>{buttonText}</button>
+                <ul className="flex flex-col gap-5 mt-8">
+                 {
+                  features.map((feature)=>(
+                    <li className="text-sm flex items-center gap-4" key={feature}>
+                      <CheckIcon className="h-6 w-6"/>
+                      <span>{feature}</span>
+                    </li>
+                  ))
+                 }
+                </ul>
+              </div>
+            ))
+           }
+          </div>
+      </div>
+    </section>
+  )
 };
--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,9 +1,11 @@
 import productImage from "@/assets/product-image.png";
+import pyramidImage from "@/assets/pyramid.png";
 import Image from "next/image";
+import tubeImage from "@/assets/tube.png";
 
 export const ProductShowcase = () => {
   return(
-    <section className="bg-gradient-to-b from-[#ffffff] to-[#d2dcff] py-24">
+    <section className="bg-gradient-to-b from-[#ffffff] to-[#d2dcff] py-24 overflow-x-clip">
       <div className="container">
         <div className="max-w-[540px] mx-auto">
         <div className="flex justify-center" >
@@ -11,13 +13,17 @@ export const ProductShowcase = () => {
             Enhance Your Productivity
           </div>
         </div>
-        <h2 className="text-center text-3xl font-bold tracking-tighter bg-gradient-to-b from-black to-green-500 text-transparent bg-clip-text mt-5 ">An efficient method to monitor progress</h2>
+        <h2 className="section-title mt-5 ">An efficient method to monitor progress</h2>
         <p className="text-center text-[22px] leading-[30px] tracking-tight text-green-950">
           Embrace new technology to grow your business, 
           save time, reach more customers, and stay competitive
         </p>
         </div>
-        <Image src={productImage} alt="Product Image" className="" />
+        <div className="relative">
+        <Image src={productImage} alt="Product Image" className="mt-10" />
+        <Image src={pyramidImage} alt="Product Image" height={265} width={262} className="hidden md:block absolute -right-36 -top-32"/>
+        <Image src={tubeImage} alt="Product Image" height={248} className="hidden md:block absolute bottom-24 -left-36"/>
+        </div>
       </div>
     </section>
   )
