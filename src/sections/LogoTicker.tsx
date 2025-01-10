eea62b2ddf60d27d103d82f92b3c9eb7e63c27fc
swaat <izunwaonu2@gmail.com>
2025-01-10 13:54:36 +0700

    Added the Logo Ticker Section

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,25 @@
+import bvLogo from "@/assets/bvlogo.png";
+import chrisLogo from "@/assets/chrislogo.png";
+import enaxLogo from "@/assets/enaxlogo.png";
+import tcmLogo from "@/assets/tcmlogo.png";
+import oliviaLogo from "@/assets/olivalogo.png";
+import celestialLogo from "@/assets/logo-celestial.png";
+import Image from "next/image";
 export const LogoTicker = () => {
-  return null;
+  return (
+    <div className="py-8 md:py-12 bg-white">
+      <div className="container">
+       <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,_transparent,_black,_transparent)]">
+          <div className="flex gap-14 flex-none">
+                <Image src={bvLogo} alt="BV Logo" className="logo-ticker-image" />
+                <Image src={enaxLogo} alt="Enax Logo" className="logo-ticker-image" />
+                <Image src={tcmLogo} alt="TCM Logo" className="logo-ticker-image" />
+                <Image src={oliviaLogo} alt="Olivia Logo" className="logo-ticker-image" />
+                <Image src={celestialLogo} alt="Celestial Logo" className="logo-ticker-image" />
+                <Image src={chrisLogo} alt="Chris Logo" className="logo-ticker-image" /> 
+            </div>
+       </div>
+      </div>
+    </div>
+  )
 };
