5aeb4b2ba612574f6f43bfdc2d15d83c8623fc55
swaat <izunwaonu2@gmail.com>
2025-01-12 02:51:11 +0700

    updated

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -3,11 +3,24 @@ import ArrowIcon from "@/assets/arrow-right.svg";
 import Image from "next/image";
 import cogImage from "@/assets/cog.png";
 import cogMain from "@/assets/cogmain.png";
-import {motion} from "framer-motion";
+import {motion, useScroll, useTransform, useMotionValueEvent} from "framer-motion";
+import { useRef } from "react";
 
 export const Hero = () => {
+
+  const heroRef = useRef(null);
+
+  const {scrollYProgress} = useScroll({
+      target: heroRef,
+      offset: ["start end", "end start"],
+  })
+
+  const translateY = useTransform(scrollYProgress, [0,1], [150, -150])
+
+  
+
   return (
-    <section className="pr-5 pt-8 pb-20 md:p-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip">
+    <section ref={heroRef} className="pr-5 pt-8 pb-20 md:p-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip">
       <div className="container">
         <div className="md:flex items-center">
           <div className="w-[320px] md:w-[486px]"> 
@@ -39,8 +52,17 @@ export const Hero = () => {
             ease: "easeInOut"
           }}
           />
-          <Image src={cogImage} alt="Img" width={220} height={220} className="hidden md:block top-16 -left-32 md:absolute"/>
-          <Image src={cogImage} alt="Img" width={220} className="lg:block absolute top-[524px] left-[448px] rotate-[30deg]"/>
+          <motion.img src={cogImage.src} alt="Img" width={220} height={220} className="hidden md:block top-16 -left-32 md:absolute"
+          style={{
+            translateY: translateY,
+          }}
+          />
+          <motion.img src={cogImage.src} alt="Img" width={220} className="lg:block absolute top-[524px] left-[448px] rotate-[30deg]"
+           style={{
+            rotate: 30,
+            translateY: translateY,
+          }}
+          />
         </div>
         </div>
       </div>
--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,4 @@
+"use client"
 import bvLogo from "@/assets/bvlogo.png";
 import chrisLogo from "@/assets/chrislogo.png";
 import enaxLogo from "@/assets/enaxlogo.png";
@@ -5,19 +6,39 @@ import tcmLogo from "@/assets/tcmlogo.png";
 import oliviaLogo from "@/assets/olivalogo.png";
 import celestialLogo from "@/assets/logo-celestial.png";
 import Image from "next/image";
+import {motion} from "framer-motion";
 export const LogoTicker = () => {
   return (
     <div className="py-8 md:py-12 bg-white">
       <div className="container">
        <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,_transparent,_black,_transparent)]">
-          <div className="flex gap-14 flex-none">
+          <motion.div className="flex gap-14 flex-none pr-14"
+          animate={{
+            translateX: '-50%',
+          }}
+          transition={{
+            duration: 20,
+            repeat: Infinity,
+            ease: 'linear',
+            repeatType: "loop"
+          }}
+          >
                 <Image src={bvLogo} alt="BV Logo" className="logo-ticker-image" />
                 <Image src={enaxLogo} alt="Enax Logo" className="logo-ticker-image" />
                 <Image src={tcmLogo} alt="TCM Logo" className="logo-ticker-image" />
                 <Image src={oliviaLogo} alt="Olivia Logo" className="logo-ticker-image" />
                 <Image src={celestialLogo} alt="Celestial Logo" className="logo-ticker-image" />
                 <Image src={chrisLogo} alt="Chris Logo" className="logo-ticker-image" /> 
-            </div>
+
+                {/* Second Sets of Logo */}
+
+                <Image src={bvLogo} alt="BV Logo" className="logo-ticker-image" />
+                <Image src={enaxLogo} alt="Enax Logo" className="logo-ticker-image" />
+                <Image src={tcmLogo} alt="TCM Logo" className="logo-ticker-image" />
+                <Image src={oliviaLogo} alt="Olivia Logo" className="logo-ticker-image" />
+                <Image src={celestialLogo} alt="Celestial Logo" className="logo-ticker-image" />
+                <Image src={chrisLogo} alt="Chris Logo" className="logo-ticker-image" />
+            </motion.div>
        </div>
       </div>
     </div>
